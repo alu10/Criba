243a79ca4d2f5a916cdbdc62575fa2c3e5c554dc
Salix <saulpachecotrilles.@gmail.com>
2021-02-24 20:24:33 +0100

    Javadoc v3

--- src/Criba.java
+++ src/Criba.java
@@ -1,8 +1,17 @@
 import java.util.Scanner;
-
+/**
+ * Clase para sacar los números primos mediante el metodo de la criba de erastotenes
+ * @version 1.0 24/02/2021
+ * @author Salix
+ *
+ */
 public class Criba {
 	
-	// Generar números primos de 1 a max
+	/**
+	 * Este metodo sirve para generar los números primos de 1 a max
+	 * @param max
+	 * @return
+	 */
 	public static int[] generarPrimos (int max) {
 		
 		int i,j;
@@ -11,19 +20,15 @@ public class Criba {
 			int dim = max + 1; // Tamaño del array
 			boolean[] esPrimo = new boolean[dim];
 			
-			// Inicializar el array
 			i = iniciarArray(dim, esPrimo);
 			
 			// Eliminar el 0 y el 1, que no son primos
 			esPrimo[0] = esPrimo[1] = false;
 			
-			// Criba
 			i = criba(dim, esPrimo);
 			
-			// ¿Cuántos primos hay?
 			int cuenta = cuentaPrimos(dim, esPrimo);
 			
-			// Rellenar el vector de números primos
 			return rellenaVector(dim, esPrimo, cuenta);
 			
 		} else { // max < 2
@@ -31,7 +36,13 @@ public class Criba {
 			// Vector vacío
 		}
 	}
-
+	
+	/**
+	 * Este metodo sirve para <code>inicializar el array</cobde>
+	 * @param dim  Tamaño del array
+	 * @param esPrimo array de primos
+	 * @return
+	 */
 	private static int iniciarArray(int dim, boolean[] esPrimo) {
 		int i;
 		for (i=0; i<dim; i++)
@@ -39,6 +50,12 @@ public class Criba {
 		return i;
 	}
 
+	/**
+	 * Este metodo sirve para <code>eliminar los que sean multiplos</code>
+	 * @param dim tamaño del array
+	 * @param esPrimo array de primos
+	 * @return
+	 */
 	private static int criba(int dim, boolean[] esPrimo) {
 		int i;
 		int j;
@@ -51,6 +68,13 @@ public class Criba {
 		return i;
 	}
 
+	/**
+	 * Este metodo es una extensión del metdo criba 
+	 * @param i
+	 * @param dim tamaño del array
+	 * @param esPrimo array de primos
+	 * @return 
+	 */
 	private static int eliminarMultiplosI(int i, int dim, boolean[] esPrimo) {
 		int j;
 		for (j=2*i; j<dim; j+=i)
@@ -58,6 +82,12 @@ public class Criba {
 		return j;
 	}
 
+	/**
+	 * Este metodo sirve para <code>saber cuantos primos hay</code>
+	 * @param dim
+	 * @param esPrimo
+	 * @return devuelve el numero de primos
+	 */
 	private static int cuentaPrimos(int dim, boolean[] esPrimo) {
 		int i;
 		int cuenta = 0;
@@ -68,6 +98,13 @@ public class Criba {
 		return cuenta;
 	}
 
+	/**
+	 * Este metodo sirve para <code>Rellena el array de numeros primos</code>
+	 * @param dim
+	 * @param esPrimo
+	 * @param cuenta
+	 * @return Devuelve los que son primos
+	 */
 	private static int[] rellenaVector(int dim, boolean[] esPrimo, int cuenta) {
 		int i;
 		int j;
@@ -79,9 +116,14 @@ public class Criba {
 		return primos;
 	}
 	
+	/**
+	 * Este es el metodo principal y sirve para <code>cojer los datos de entrada e imprimirlos en la consola</code>
+	 * @param args
+	 * Aqui esta la parte que sale por pantalla y 
+	 */
 	public static void main(String[] args) {
 		Scanner teclado=new Scanner(System.in);
-		System.out.println("Introduce el número para la criba de Erast�tenes:");
+		System.out.println("Introduce el número para la criba de Erastótenes:");
 		int dato=teclado.nextInt();
 		int vector[]=new int[dato];
 		System.out.println("\nVector inicial hasta :"+dato);
